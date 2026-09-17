461fa49ef04bbb03410154e6eba8b24dfcf1aad1
Charlie David <davys4802@gmail.com>
2026-09-17 12:13:23 -0500

    Entire program

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
-
 public class Empleado {
+
+    private int codigo;
+
+    public Empleado()
+    {
+        codigo = 0;
+    }
+
+    public int getCodigo()
+    {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo)
+    {
+        this.codigo = codigo;
+    }
+
+    // El código debe tener 3 cifras (100 a 999)
+    public boolean codigoValido()
+    {
+        return codigo >= 100 && codigo <= 999;
+    }
+
+    public String obtenerCategoria()
+    {
+        boolean div2 = codigo % 2 == 0;
+        boolean div3 = codigo % 3 == 0;
+        boolean div5 = codigo % 5 == 0;
+        String categoria;
+
+        if (div2 && div3 && div5)
+        {
+            categoria = "Director General";
+        }
+        else if (!div2 && div3 && div5)
+        {
+            categoria = "Directivo";
+        }
+        else if (div2 && !div3 && !div5)
+        {
+            categoria = "Staff";
+        }
+        else if (!div2 && !div3 && !div5)
+        {
+            categoria = "Seguridad";
+        }
+        else
+        {
+            categoria = "Sin categoría asignada";
+        }
+
+        return categoria;
+    }
 }
